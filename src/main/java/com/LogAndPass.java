a3f891c8d9888de16255eb9ac5731a17a167fa2d
Sidethus <68002560+Sidethus@users.noreply.github.com>
2020-12-11 20:03:51 +0400

    Initial commit

---
+++ src/main/java/com/LogAndPass.java
@@ -0,0 +1,23 @@
+package com;
+
+public class LogAndPass
+{
+    String login;
+    String password;
+
+    public String getCommon() {
+        return login;
+    }
+
+    public void setCommon(String common) {
+        this.login = common;
+    }
+
+    public String getBotanical() {
+        return password;
+    }
+
+    public void setBotanical(String botanical) {
+        this.password = botanical;
+    }
+}
